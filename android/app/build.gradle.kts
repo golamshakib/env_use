f150a13fffdfb54adef26c077611ac852a55640c
golamshakib <golamshakib.h@gmail.com>
2025-07-22 20:51:07 +0600

    added the ndkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.env.use.envuse.env_use"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
